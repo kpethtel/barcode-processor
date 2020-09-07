8a6e039c4f7facc057f1551f954d4196382ba9e8
kpethtel <kris.pethtel@gmail.com>
2020-09-07 16:26:32 -0400

    add barcode processing

--- app/controllers/barcodes_controller.rb
+++ app/controllers/barcodes_controller.rb
@@ -1,2 +1,45 @@
 class BarcodesController < ApplicationController
+  before_action :process_barcode_file, only: :upload
+
+  def import
+  end
+
+  def upload
+    if error
+      flash.alert = error
+      return render :import
+    end
+    if failures.any?
+      flash.alert = "Invalid barcodes found: #{failures.join(', ')}"
+      return render :import
+    end
+    flash.alert = "#{successes.count} barcodes imported!"
+    redirect_to :root
+  end
+
+  private
+
+  def file
+    @file ||= params.dig(:upload, :file)
+  end
+
+  def importer
+    @importer ||= BarcodeImporterService.new(file)
+  end
+
+  def process_barcode_file
+    importer.process
+  end
+
+  def successes
+    importer.barcode_statuses[:success]
+  end
+
+  def failures
+    importer.barcode_statuses[:failure]
+  end
+
+  def error
+    importer.barcode_statuses[:error]
+  end
 end
---
+++ app/services/barcode_importer_service.rb
@@ -0,0 +1,65 @@
+class BarcodeImporterService
+
+  attr_reader :file
+  attr_reader :parsed_file
+  attr_accessor :barcode_statuses
+
+  def initialize(file)
+    @file = file
+    @barcode_statuses = { success: [], failure: [], error: nil }
+    validate_file
+    validate_file_extension unless barcode_statuses[:error]
+    parse_file unless barcode_statuses[:error]
+  end
+
+  def process
+    return if barcode_statuses[:error]
+    process_barcodes
+  end
+
+  private
+
+  def validate_file
+    if !file || file.class != ActionDispatch::Http::UploadedFile
+      barcode_statuses[:error] = 'No file uploaded'
+    end
+  end
+
+  def validate_file_extension
+    if File.extname(file.path) != '.xlsx'
+      barcode_statuses[:error] = 'Uploaded file must be of type .xlsx'
+    end
+  end
+
+  def parse_file
+    @parsed_file = RubyXL::Parser.parse(file.path)
+  rescue Zip::Error => e
+    barcode_statuses[:error] = 'File error'
+  end
+
+  def worksheet
+    parsed_file[0]
+  end
+
+  def data_rows
+    @data_rows ||= worksheet.sheet_data.rows[1..-1]
+  end
+
+  def barcodes
+    @barcodes ||= data_rows.map do |row|
+      next if row[0].value.nil?
+      row[0].value.to_s
+    end.compact
+  end
+
+  def process_barcodes
+    barcodes.each do |code|
+      if EAN8.valid?(code)
+        barcode_statuses[:success] << code
+      else
+        barcode_statuses[:failure] << code
+      end
+    end
+  end
+
+end
